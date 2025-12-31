0163f3e7603c39188fd514ced001023a3d4686d5
vladislav <107300120+homelless@users.noreply.github.com>
2025-12-31 13:01:36 +0300

    Initial Commit

---
+++ MoneyFlowUp/MoneyFlowUpApp.swift
@@ -0,0 +1,32 @@
+//
+//  MoneyFlowUpApp.swift
+//  MoneyFlowUp
+//
+//  Created by MacBookAir on 31.12.25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MoneyFlowUpApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
